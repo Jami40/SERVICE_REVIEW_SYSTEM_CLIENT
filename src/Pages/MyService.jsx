f14f57f265712efe41e4c6f578dce3ae11e1ecb5
Jami40 <jnjami40@gmail.com>
2024-12-24 03:25:20 +0600

    My service Page added

--- src/Pages/MyService.jsx
+++ src/Pages/MyService.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import axios from 'axios';
 
 const MyService = () => {
+    const {user}=useContext(AuthContext)
+    const [services,setServices]=useState([])
+    useEffect(()=>{
+        // fetch(`http://localhost:5000/service/myFav?email=${user.email}`)
+        // .then(res=>res.json())
+        // .then(data=>setServices(data))
+        axios.get(`http://localhost:5000/service/myFav?email=${user.email}`)
+        .then(res=>setServices(res.data))
+        .catch(err=>console.log(err))
+    },[user.email])
     return (
         <div>
+            <h2 className='text-white'>Services:{services.length}</h2>
             
         </div>
     );
